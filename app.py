f2381a671cdb3973bc62ce1da86ca05b052aa66d
kareem-xyz <kareemrashad.sd@gmail.com>
2025-02-23 17:30:09 -0500

    Somehow this works

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 from flask_session import Session
 from concurrent.futures import ThreadPoolExecutor
-from helpers import * # import all helper function from helper.py
+from helpers import * # import all helper functions from helpers.py
 import json
 
 # Configure Application
@@ -10,11 +10,10 @@ executor = ThreadPoolExecutor()
 
 # Set the secret key for session
 app.secret_key = '_5#y2LF4Qsbfiqbf2768er4vef6acg7egt7b9rgv634vbt7fgve'
-app.config['SESSION_TYPE'] = 'filesystem' # use filesystem storage (server storage)
+app.config['SESSION_TYPE'] = 'filesystem'  # use filesystem storage (server storage)
 app.config['SESSION_PERMANENT'] = False
-app.config["TEMPLATES_AUTO_RELOAD"] = True # Ensure templates are auto-reloaded
-Session(app) 
-
+app.config["TEMPLATES_AUTO_RELOAD"] = True  # Ensure templates are auto-reloaded
+Session(app)
 
 @app.route('/test')
 def test():
@@ -29,7 +28,7 @@ def index():
     return render_template('layout.html')
     
 @app.route('/search')
-def search():
+async def search():
     # Using GET (also from forms) if I used POST I would have used request.form.get
     input_0 = request.args.get("movie0")
     input_1 = request.args.get("movie1")
@@ -39,28 +38,25 @@ def search():
         return redirect('/')
     
     # Movie Databases Api to get Movie's Data.
-    # https://rapidapi.com/SAdrian/api/moviesdatabase/
-
-    # Api Specifics (login key and query parameters)
-    querystring = {"exact":"false","titleType":"movie", "info":"custom_info"}
+    querystring = {"exact": "false", "titleType": "movie", "info": "custom_info"}
 
     # Run Api
-    data_0 = get_movie_info(title=input_0, params=querystring)
-    data_1 = get_movie_info(title=input_1, params=querystring)
+    data_0 = await get_movie_info(title=input_0, params=querystring)
+    data_1 = await get_movie_info(title=input_1, params=querystring)
 
     # Format Movies for sending (jsonifying and sorting)
     if data_0:
-        data_0 = sorted(data_0, key=lambda x : (x['primaryImage'] is not None, x['ratingsSummary']['voteCount']), reverse=True)
+        data_0 = sorted(data_0, key=lambda x: (x['primaryImage'] is not None, x['ratingsSummary']['voteCount']), reverse=True)
         data_0 = replace_quotes(data_0)
 
     if data_1:
-        data_1 = sorted(data_1, key=lambda x : (x['primaryImage'] is not None, x['ratingsSummary']['voteCount']), reverse=True)
+        data_1 = sorted(data_1, key=lambda x: (x['primaryImage'] is not None, x['ratingsSummary']['voteCount']), reverse=True)
         data_1 = replace_quotes(data_1)
             
     return render_template('search.html', datalist_0=data_0, datalist_1=data_1)
 
 @app.route("/fight", methods=['POST'])
-def fight():
+async def fight():
     # Get list of IDs for director, creators, writers, and cast of each movie
     cast_0 = request.form.get('cast_0')
     cast_1 = request.form.get('cast_1')
@@ -68,8 +64,8 @@ def fight():
     # Check for invalid input
     if not (cast_0 and cast_1):
         return render_template('bug.html', bug='Did not receive cast list for both movies. ERROR 100')
-    try :
-        # Since values are a single line of csv strings. We can just split.
+
+    try:
         cast_0 = cast_0.split(',')
         cast_1 = cast_1.split(',')
     except Exception as e:
@@ -77,62 +73,46 @@ def fight():
 
     # Run API to Get ACTORS data Use asyncio to run the asynchronous function for both lists concurrently
     try:
-        list_0, list_1 = run_async(get_all_actors_info(cast_0)), run_async(get_all_actors_info(cast_1))
+        list_0, list_1 = await asyncio.gather(get_all_actors_info(cast_0), get_all_actors_info(cast_1))
     except Exception as e:
         return render_template('bug.html', bug=f'Error fetching actor info: {str(e)}')
-    
-    # UNNECSSARY AFTER MADE FUNCTION FOR BELOW, Faced some trouble so might fall back
-    # Run API to Get Movies of Actors
-    # movies_IDs_1 = []
-    # for index, actor in enumerate(list_0):
-    #     try:
-    #         movies_IDs_0 += (actor['results']['knownForTitles']).split(',')
-    #     except Exception as e:
-    #         print(f'Error at index {index}')
-    #         print(e)
-    #         continue
+
     movies_IDs_0 = get_actors_movies(list_0)
     movies_IDs_1 = get_actors_movies(list_1)
 
-    # Remove duplicates from  each list (works by converting to dict and back to list)
-    movies_IDs_0 = list(dict.fromkeys(movies_IDs_0))
-    movies_IDs_1 = list(dict.fromkeys(movies_IDs_1))
+    # Remove duplicates from each list (works by converting to dict and back to list)
+    movies_IDs_0 = list(dict.fromkeys(await movies_IDs_0))
+    movies_IDs_1 = list(dict.fromkeys(await movies_IDs_1))
 
     # Remove overlapping movies from lists (I dont want to ask about a movie if it will add points to both movies)
     movies_IDs_0 = remove_overlap(movies_IDs_0, movies_IDs_1)
     movies_IDs_1 = remove_overlap(movies_IDs_1, movies_IDs_0)
 
-    # Run API for  Movies Data
+    # Run API for Movies Data
     data_0 = get_all_movies_info(IDs_list=movies_IDs_0)
     data_1 = get_all_movies_info(IDs_list=movies_IDs_1)
-    
-    # Format JSON correctly.
-    data_0 = replace_quotes(data_0) 
-    data_1 = replace_quotes(data_1) 
 
-    # session used to access data across different flask routes without using URL parameters (big files dont work well with URLs)
-    session['m0_similarMovies'] = data_0 
+    # Format JSON correctly.
+    data_0 = replace_quotes(data_0)
+    data_1 = replace_quotes(data_1)
+
+    # session used to access data across different flask routes without using URL parameters (big files don't work well with URLs)
+    session['m0_similarMovies'] = data_0
     session['m1_similarMovies'] = data_1
         
-    # return render_template('fight.html', questions=questions_json['questions'], m0_similarMovies=data_0, m1_similarMovies=data_1)
-    # Now rendering via a different route to simplify GET requests and refreshes.
     return redirect(url_for('result'))
 
 @app.route('/result', methods=['GET'])
 def result():
-    # Pass the questions file too.
     with open('static/assets/questions.json', 'r') as f:
         questions_json = json.load(f)
-    q =  questions_json['questions']
+    q = questions_json['questions']
     s0 = session.get('m0_similarMovies')
     s1 = session.get('m1_similarMovies')
 
-    # if session data cleared go to homepage
-    if (not s0 or not s1):
-        if not (s0 and s1):
-            return redirect('index')
-        return render_template('bug.html', bug='Did not find list of similar movies for one of the movies, OR movie data is cleared')
-    
+    if not s0 or not s1:
+        return redirect('index')
+
     return render_template('fight.html', questions=q, m0_similarMovies=s0, m1_similarMovies=s1)
 
 @app.route('/winner', methods=['POST'])
--- helpers.py
+++ helpers.py
@@ -1,53 +1,51 @@
-import requests # Used in the query Api
 import httpx
 import asyncio
 
-# Used in each Api request.
+# Used in each API request.
 headers = {
-        "X-RapidAPI-Key": "dcabfff8b1msh47092185488eb22p1b47e2jsn45e1e47ab1f7",
-        "X-RapidAPI-Host": "moviesdatabase.p.rapidapi.com"
-    }
+    "X-RapidAPI-Key": "dcabfff8b1msh47092185488eb22p1b47e2jsn45e1e47ab1f7",
+    "X-RapidAPI-Host": "moviesdatabase.p.rapidapi.com"
+}
 
+# Run async functions with asyncio.run
 def run_async(func):
     return asyncio.run(func)
 
+# Get Data for one movie (synchronous)
+async def get_movie_info(title, params):
+    base_url = 'https://moviesdatabase.p.rapidapi.com/titles/search/title/'
+    async with httpx.AsyncClient() as client:
+        response = await client.get(base_url + title, headers=headers, params=params)
+        return response.json()['results']
 
-# get Data for one movie. Takes movie title and parameters as input.
-def get_movie_info(title, params):
-    base_url='https://moviesdatabase.p.rapidapi.com/titles/search/title/'
-    response = requests.get(base_url + title, headers=headers, params=params)
-    # Convert to json and return only the results
-    return (response.json())['results']
-
-
-# Data for list of Movies. Takes list of cast IDs.
-def get_all_movies_info(IDs_list):
-    base_url='https://moviesdatabase.p.rapidapi.com/titles/x/titles-by-ids'
+# Get Data for a list of Movies (synchronous)
+async def get_all_movies_info(IDs_list):
+    base_url = 'https://moviesdatabase.p.rapidapi.com/titles/x/titles-by-ids'
     csv_IDs_list = ','.join(IDs_list)
     querystring = {"idsList": csv_IDs_list, 'info': 'custom_info'}
-    response = requests.get(base_url, headers=headers, params=querystring)
-    # Convert to json and return only the results
-    return (response.json())['results']
+    async with httpx.AsyncClient() as client:
+        response = await client.get(base_url, headers=headers, params=querystring)
+        return response.json()['results']
 
-# Given a list of actors data, find the movies for each actors and return a list of movie IDs
-def get_actors_movies(lst):
+# Given a list of actors' data, find the movies for each actor and return a list of movie IDs
+async def get_actors_movies(lst):
     movie_ID_list = []
     for index, actor in enumerate(lst):
         try:
-            movie_ID_list += (actor['results']['knownForTitles']).split(',')
+            # Assuming actor['results']['knownForTitles'] is a string of movie IDs
+            movie_ID_list += actor['results']['knownForTitles'].split(',')
         except Exception as e:
             print(f'Error at index {index}')
             print(e)
             continue
     return movie_ID_list
-        
-# Data for list of actors. Asyncio. Takes list of cast IDs.
+
+# Data for list of actors (async), takes list of cast IDs
 async def get_all_actors_info(cast_list):
     coroutines = [get_actor_info(actor_id) for actor_id in cast_list]
     return await asyncio.gather(*coroutines)
-    
 
-# Data for One actor. Asyncio Takes a single actor ID.
+# Data for One actor (async), takes a single actor ID
 async def get_actor_info(actor_id):
     base_url = 'https://moviesdatabase.p.rapidapi.com/actors/'
     async with httpx.AsyncClient() as client:
@@ -64,16 +62,14 @@ def replace_quotes(obj):
         return obj.replace('"', "'")
     else:
         return obj
-    
+
+# Remove overlap between two lists
 def remove_overlap(list0, list1):
-    # Convert lists to sets
     set0 = set(list0)
     set1 = set(list1)
 
-    # Find unique strings in set
+    # Find unique strings in set0 that are not in set1
     unique_list0 = set0 - set1
 
-    # Convert sets back to lists
-    unique_list0 = list(unique_list0)
-
-    return unique_list0
+    # Convert set back to list
+    return list(unique_list0)
